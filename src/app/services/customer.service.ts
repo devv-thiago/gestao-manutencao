bbf1f6b6f41ebb31403868bbd742972e43456f2b
devv-thiago <tfofano3@gmail.com>
2025-06-20 18:42:00 -0300

    adicionando esqueleto dos servicos

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/contatos';
+
+  constructor(private http: HttpClient) { }
+
+  // GET
+  getContatos(): Observable<number[]> {
+    return this.http.get<number[]>(this.apiUrl);
+  }
+
+  // POST
+  criarContato(contato: number): Observable<number> {
+    return this.http.post<number>(this.apiUrl, contato);
+  }
+
+  // PUT
+  atualizarContato(contato: number): Observable<number> {
+    return this.http.put<number>(`${this.apiUrl}/${contato}`, contato);
+  }
+
+  // DELETE
+  deletarContato(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
+private apiUrl = 'http://localhost:3000/contatos';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // GET
+  getContatos(): Observable<number[]> {
+    return this.http.get<number[]>(this.apiUrl);
+  }
+
+  // POST
+  criarContato(contato: number): Observable<number> {
+    return this.http.post<number>(this.apiUrl, contato);
+  }
+
+  // PUT
+  atualizarContato(contato: number): Observable<number> {
+    return this.http.put<number>(`${this.apiUrl}/${contato}`, contato);
+  }
+
+  // DELETE
+  deletarContato(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
